57297765edf21ec99cf26704bf06c6fd68259279
caleressmisu <missun1201@me.com>
2024-11-24 18:18:10 +0900

    15-forms

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,31 +1,18 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   template: `
     <p>Username: {{ username }}</p>
-    <p>Preferred Framework:</p>
-    <ul>
-      <li>
-        Static Image:
-        <img
-          ngSrc="/assets/logo.svg"
-          alt="Angular logo"
-          width="32"
-          height="32"
-        />
-      </li>
-      <li>
-        Dynamic Image:
-        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32" />
-      </li>
-    </ul>
+    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <label for="framework">Favorite Framework:
+      <input id="framework" type="text" [(ngModel)]="favoriteFramework"/>
+    </label>
   `,
-  imports: [NgOptimizedImage],
+  imports: [FormsModule],
 })
 export class UserComponent {
-  logoUrl = '/assets/logo.svg';
-  logoAlt = 'Angular logo';
   username = 'youngTech';
+  favoriteFramework = '';
 }
