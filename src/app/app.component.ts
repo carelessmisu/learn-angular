6ada70b17609a1afe197f6da6f73f317b4d468c3
caleressmisu <missun1201@me.com>
2024-11-24 18:02:25 +0900

    12-enable-routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import {Component} from '@angular/core';
-import {UserComponent} from './user.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-user />
+    <nav>
+      <a href="/">Home</a>
+      |
+      <a href="/user">User</a>
+    </nav>
+    <router-outlet />
   `,
-  imports: [UserComponent],
+  imports: [RouterOutlet],
 })
 export class AppComponent {}
