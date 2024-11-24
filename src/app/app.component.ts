1f96603b4105b5fad4115759547821409c86b3b8
caleressmisu <missun1201@me.com>
2024-11-24 16:44:22 +0900

    7-event-handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
   template: `
-    <div [contentEditable]="isEditable"></div>
+    <section (mouseover)="onMouseOver()">
+      There's a secret message for you, hover to reveal 👀
+      {{ message }}
+    </section>
   `,
 })
 export class AppComponent {
-  isEditable = true;
+  message = '';
+
+  onMouseOver() {
+    this.message = 'Way to go 🚀';
+  }
 }
