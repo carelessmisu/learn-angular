7e323496ee2017e784a046ec79ce0aa3cc35da91
caleressmisu <missun1201@me.com>
2024-11-27 21:04:19 +0900

    24-create-a-pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
-import { DecimalPipe, DatePipe, CurrencyPipe } from '@angular/common';
+import { ReversePipe } from './reverse.pipe';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <ul>
-      <li>Number with "decimal" {{ num | number:"3.2-2"}}</li>
-      <li>Date with "date" {{ birthday | date: 'medium'}}</li>
-      <li>Currency with "currency" {{ cost | currency}}</li>
-    </ul>
-  `,
-  imports: [DecimalPipe, DatePipe, CurrencyPipe],
+  template: ` Reverse Machine: {{ word | reverse}} `,
+  imports: [ReversePipe],
 })
 export class AppComponent {
-  num = 103.1234;
-  birthday = new Date(2023, 3, 2);
-  cost = 4560.34;
+  word = 'You are a champion';
 }
